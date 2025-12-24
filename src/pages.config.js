bd37b514afc292a8a26c4f3879886d166aaebfc2
Handy Planet <okan.yzci@gmail.com>
2025-12-24 22:04:26 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -4,28 +4,28 @@ import Datenschutz from './pages/Datenschutz';
 import Home from './pages/Home';
 import Impressum from './pages/Impressum';
 import Kontakt from './pages/Kontakt';
+import Leistungen from './pages/Leistungen';
 import Preise from './pages/Preise';
+import Ratgeber from './pages/Ratgeber';
 import UeberUns from './pages/UeberUns';
+import androidBootloopStartetNicht from './pages/android-bootloop-startet-nicht';
+import androidDatenrettung from './pages/android-datenrettung';
+import displaySchadenDatenrettung from './pages/display-schaden-datenrettung';
+import geloeschteDatenWiederherstellen from './pages/geloeschte-daten-wiederherstellen';
+import geloeschteFotosWiederherstellen from './pages/geloeschte-fotos-wiederherstellen';
 import handyDatenRettungGraz from './pages/handy-daten-rettung-graz';
 import handyDatenRettungInnsbruck from './pages/handy-daten-rettung-innsbruck';
 import handyDatenRettungLinz from './pages/handy-daten-rettung-linz';
 import handyDatenRettungSalzburg from './pages/handy-daten-rettung-salzburg';
 import handyDatenRettungWien from './pages/handy-daten-rettung-wien';
-import Leistungen from './pages/Leistungen';
-import iphoneDatenrettung from './pages/iphone-datenrettung';
-import samsungDatenrettung from './pages/samsung-datenrettung';
-import androidDatenrettung from './pages/android-datenrettung';
-import wasserschadenDatenrettung from './pages/wasserschaden-datenrettung';
-import displaySchadenDatenrettung from './pages/display-schaden-datenrettung';
-import platinenschadenDatenrettung from './pages/platinenschaden-datenrettung';
-import geloeschteDatenWiederherstellen from './pages/geloeschte-daten-wiederherstellen';
-import iphoneVsSamsungDatenrettung from './pages/iphone-vs-samsung-datenrettung';
-import geloeschteFotosWiederherstellen from './pages/geloeschte-fotos-wiederherstellen';
-import handyStartetNicht from './pages/handy-startet-nicht';
-import Ratgeber from './pages/Ratgeber';
-import handyWasserschadenDatenrettung from './pages/handy-wasserschaden-datenrettung';
 import handyDisplayDefektDatenwichtig from './pages/handy-display-defekt-datenwichtig';
-import androidBootloopStartetNicht from './pages/android-bootloop-startet-nicht';
+import handyStartetNicht from './pages/handy-startet-nicht';
+import handyWasserschadenDatenrettung from './pages/handy-wasserschaden-datenrettung';
+import iphoneDatenrettung from './pages/iphone-datenrettung';
+import iphoneVsSamsungDatenrettung from './pages/iphone-vs-samsung-datenrettung';
+import platinenschadenDatenrettung from './pages/platinenschaden-datenrettung';
+import samsungDatenrettung from './pages/samsung-datenrettung';
+import wasserschadenDatenrettung from './pages/wasserschaden-datenrettung';
 import __Layout from './Layout.jsx';
 
 
@@ -36,28 +36,28 @@ export const PAGES = {
     "Home": Home,
     "Impressum": Impressum,
     "Kontakt": Kontakt,
+    "Leistungen": Leistungen,
     "Preise": Preise,
+    "Ratgeber": Ratgeber,
     "UeberUns": UeberUns,
+    "android-bootloop-startet-nicht": androidBootloopStartetNicht,
+    "android-datenrettung": androidDatenrettung,
+    "display-schaden-datenrettung": displaySchadenDatenrettung,
+    "geloeschte-daten-wiederherstellen": geloeschteDatenWiederherstellen,
+    "geloeschte-fotos-wiederherstellen": geloeschteFotosWiederherstellen,
     "handy-daten-rettung-graz": handyDatenRettungGraz,
     "handy-daten-rettung-innsbruck": handyDatenRettungInnsbruck,
     "handy-daten-rettung-linz": handyDatenRettungLinz,
     "handy-daten-rettung-salzburg": handyDatenRettungSalzburg,
     "handy-daten-rettung-wien": handyDatenRettungWien,
-    "Leistungen": Leistungen,
-    "iphone-datenrettung": iphoneDatenrettung,
-    "samsung-datenrettung": samsungDatenrettung,
-    "android-datenrettung": androidDatenrettung,
-    "wasserschaden-datenrettung": wasserschadenDatenrettung,
-    "display-schaden-datenrettung": displaySchadenDatenrettung,
-    "platinenschaden-datenrettung": platinenschadenDatenrettung,
-    "geloeschte-daten-wiederherstellen": geloeschteDatenWiederherstellen,
-    "iphone-vs-samsung-datenrettung": iphoneVsSamsungDatenrettung,
-    "geloeschte-fotos-wiederherstellen": geloeschteFotosWiederherstellen,
-    "handy-startet-nicht": handyStartetNicht,
-    "Ratgeber": Ratgeber,
-    "handy-wasserschaden-datenrettung": handyWasserschadenDatenrettung,
     "handy-display-defekt-datenwichtig": handyDisplayDefektDatenwichtig,
-    "android-bootloop-startet-nicht": androidBootloopStartetNicht,
+    "handy-startet-nicht": handyStartetNicht,
+    "handy-wasserschaden-datenrettung": handyWasserschadenDatenrettung,
+    "iphone-datenrettung": iphoneDatenrettung,
+    "iphone-vs-samsung-datenrettung": iphoneVsSamsungDatenrettung,
+    "platinenschaden-datenrettung": platinenschadenDatenrettung,
+    "samsung-datenrettung": samsungDatenrettung,
+    "wasserschaden-datenrettung": wasserschadenDatenrettung,
 }
 
 export const pagesConfig = {
